6de596d4c3983947207021fb393f25f972a4c49c
FtxDante <blog.anutrof@gmail.com>
2022-01-12 20:56:59 -0300

    feat branch with orm config

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,6 +1,8 @@
 import {createConnection} from 'typeorm';
+import {createDatabase} from 'typeorm-extension';
 
 export const connection = async () => {
   const connecionName = process.env.NODE_ENV == 'test'? 'test': 'default';
+  await createDatabase({ifNotExist: true, characterSet: 'UTF8'});
   await createConnection(connecionName);
 };
