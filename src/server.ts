c030ad7a4612c7ee540ef3145a1cf3de0aef6112
FtxDante <blog.anutrof@gmail.com>
2022-01-15 16:58:10 -0300

    ref(server) added env PORT for heroku

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
-const port = 3000;
+const port: number = parseInt(process.env.PORT!);
 
-app.listen(port, () => {
+app.listen(port | 3000, () => {
   console.log(`The server is running at port ${port}`);
 });
